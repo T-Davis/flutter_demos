5d49695d58c4af54ddd2d4c2567c1f665023d35a
Trevor Davis <tzendou@gmail.com>
2020-06-22 14:47:08 -0700

    Add functionality

--- magic-8-ball-flutter/lib/main.dart
+++ magic-8-ball-flutter/lib/main.dart
@@ -1,7 +1,43 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.red,
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+            backgroundColor: Colors.blue.shade700,
+          ),
+          body: AskMeAnything(),
+        ),
       ),
     );
+
+class AskMeAnything extends StatefulWidget {
+  @override
+  _AskMeAnythingState createState() => _AskMeAnythingState();
+}
+
+class _AskMeAnythingState extends State<AskMeAnything> {
+  int ballNumber;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue.shade300,
+      child: Center(
+        child: FlatButton(
+          child: Image.asset('images/ball$ballNumber.png'),
+          highlightColor: Colors.transparent,
+          splashColor: Colors.transparent,
+          onPressed: () {
+            setState(() {
+              ballNumber = Random().nextInt(5) + 1;
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
