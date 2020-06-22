76fa342d184e718fa945dabfe5ce5da438258b4e
Trevor Davis <tzendou@gmail.com>
2020-06-22 12:08:28 -0700

    Add dice as buttons

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -18,6 +18,23 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Center(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                child: Image.asset('images/dice1.png'),
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                child: Image.asset('images/dice1.png'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
