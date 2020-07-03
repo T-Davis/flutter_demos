641c87e95c171621a477c812fdd6b2d8e536cd10
Trevor Davis <tzendou@gmail.com>
2020-07-02 23:29:06 -0700

    Add app layout

--- bmi-calculator-flutter/lib/input_page.dart
+++ bmi-calculator-flutter/lib/input_page.dart
@@ -9,14 +9,52 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+                child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Card(color: Color(0xFF1E2032)),
+                ),
+                Expanded(
+                  child: Card(color: Color(0xFF1E2032)),
+                )
+              ],
+            )),
+            Expanded(
+              child: Card(color: Color(0xFF1E2032)),
+            ),
+            Expanded(
+                child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Card(color: Color(0xFF1E2032)),
+                ),
+                Expanded(
+                  child: Card(color: Color(0xFF1E2032)),
+                )
+              ],
+            ))
+          ],
+        ));
+  }
+}
+
+class Card extends StatelessWidget {
+  final Color color;
+  Card({@required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: this.color,
+        borderRadius: BorderRadius.circular(10),
       ),
     );
   }
