6b721e4ac693cdeef30cc0fc8e7c9f0691d51c40
Trevor Davis <tzendou@gmail.com>
2020-07-04 23:44:47 -0700

    Add male and female cards

--- bmi-calculator-flutter/lib/input_page.dart
+++ bmi-calculator-flutter/lib/input_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const double bottomContainerHeight = 80;
 const selectableCardColor = Color(0xFF1E2032);
 const notSelectableCardColor = Color(0xFF101527);
 const bottomContainerColor = Color(0xFFEA1556);
+const unselectedIconColor = Color(0xFF8E8E99);
 
 class InputPage extends StatefulWidget {
   @override
@@ -23,10 +25,22 @@ class _InputPageState extends State<InputPage> {
               child: Row(
             children: <Widget>[
               Expanded(
-                child: Card(color: selectableCardColor),
+                child: Card(
+                  color: selectableCardColor,
+                  child: IconContent(
+                    icon: FontAwesomeIcons.mars,
+                    text: 'MALE',
+                  ),
+                ),
               ),
               Expanded(
-                child: Card(color: selectableCardColor),
+                child: Card(
+                  color: selectableCardColor,
+                  child: IconContent(
+                    icon: FontAwesomeIcons.venus,
+                    text: 'FEMALE',
+                  ),
+                ),
               )
             ],
           )),
@@ -61,14 +75,47 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+class IconContent extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  const IconContent({this.icon, this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          icon,
+          size: 80,
+          color: unselectedIconColor,
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Text(
+          text,
+          style: TextStyle(
+            fontSize: 18,
+            color: unselectedIconColor,
+          ),
+        )
+      ],
+    );
+  }
+}
+
 class Card extends StatelessWidget {
   final Color color;
+  final Widget child;
 
-  Card({@required this.color});
+  const Card({@required this.color, this.child});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      child: child,
       margin: EdgeInsets.all(15),
       decoration: BoxDecoration(
         color: this.color,
