b95ecf28510d68ffaedd73d8215ac69b7d8daeb9
Trevor Davis <tzendou@gmail.com>
2020-07-12 23:57:18 -0700

    Setup networking class

--- clima-weather-app/lib/screens/loading_screen.dart
+++ clima-weather-app/lib/screens/loading_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+
+const String apiKey = 'b390e706a11e93ff2d47b71244536ea5';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -8,6 +12,8 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
   @override
   void initState() {
     super.initState();
@@ -17,16 +23,25 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.longitude);
-    print(location.latitude);
+    longitude = location.longitude;
+    latitude = location.latitude;
+    getData();
   }
 
   void getData() async {
     http.Response response = await http.get(
-        'https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02');
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
 
     if (response.statusCode == 200) {
-      print(response.body);
+      var decodedData = jsonDecode(response.body);
+
+      double temperature = decodedData['main']['temp'];
+      int condition = decodedData['weather'][0]['id'];
+      String cityName = decodedData['name'];
+
+      print(temperature);
+      print(condition);
+      print(cityName);
     } else {
       print(response.statusCode);
     }
@@ -34,7 +49,6 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    getData();
     return Scaffold(
       body: Center(
         child: RaisedButton(
--- i_am_rich/android/app/src/main/kotlin/com/tdavis/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.tdavis
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
