ccf1334c2d930b7845f28958abb3f71fb49714cd
Trevor Davis <tzendou@gmail.com>
2020-07-18 16:35:11 -0700

    Add currencies and cryptos

---
+++ crypto_price_ticker/lib/utils/constants.dart
@@ -0,0 +1,29 @@
+const List<String> currencies = [
+  'AUD',
+  'BRL',
+  'CAD',
+  'CNY',
+  'EUR',
+  'GBP',
+  'HKD',
+  'IDR',
+  'ILS',
+  'INR',
+  'JPY',
+  'MXN',
+  'NOK',
+  'NZD',
+  'PLN',
+  'RON',
+  'RUB',
+  'SEK',
+  'SGD',
+  'USD',
+  'ZAR'
+];
+
+const List<String> cryptos = [
+  'BTC',
+  'ETH',
+  'LTC',
+];
