c3dde256ee90476171e3dc35a4b4d8916e547c32
Trevor Davis <tzendou@gmail.com>
2020-07-20 18:59:51 -0700

    Update GET request to return list of Crypto

--- crypto_price_ticker/lib/network.dart
+++ crypto_price_ticker/lib/network.dart
@@ -1,18 +1,44 @@
 import 'dart:convert';
 
+import 'package:cryptopriceticker/models/crypto.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
 class Network {
-  static const url = "https://rest.coinapi.io/v1/exchangerate/BTC/";
-  static Future getPrices(String currency) async {
-    var header = {'X-CoinAPI-Key': '${DotEnv().env['API_KEY']}'};
-    http.Response response = await http.get(url + currency, headers: header);
+  static const url = "https://rest.coinapi.io/v1/exchangerate/";
 
-    if (response.statusCode == 200) {
-      return jsonDecode(response.body);
+  static Future<List<Crypto>> getPrices(String currency) async {
+    List<Crypto> cryptos = [];
+
+    var header = {'X-CoinAPI-Key': '${DotEnv().env['API_KEY']}'};
+
+    http.Response btc =
+        await http.get(url + 'BTC/' + currency, headers: header);
+    if (btc.statusCode == 200) {
+      var res = jsonDecode(btc.body);
+      cryptos.add(Crypto(name: 'BTC', price: res['rate']));
     } else {
-      print(response.statusCode);
+      cryptos.add(Crypto(name: 'BTC', price: 0.00));
     }
+
+    http.Response eth =
+        await http.get(url + 'ETH/' + currency, headers: header);
+    if (eth.statusCode == 200) {
+      var res = jsonDecode(btc.body);
+      cryptos.add(Crypto(name: 'ETH', price: res['rate']));
+    } else {
+      cryptos.add(Crypto(name: 'ETH', price: 0.00));
+    }
+
+    http.Response ltc =
+        await http.get(url + 'LTC/' + currency, headers: header);
+    if (ltc.statusCode == 200) {
+      var res = jsonDecode(btc.body);
+      cryptos.add(Crypto(name: 'LTC', price: res['rate']));
+    } else {
+      cryptos.add(Crypto(name: 'LTC', price: 0.00));
+    }
+
+    return cryptos;
   }
 }
