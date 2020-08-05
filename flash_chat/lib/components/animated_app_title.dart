d74bcd0fa2c5983364ba0b9092e10bb063d24681
Trevor Davis <tzendou@gmail.com>
2020-08-05 15:01:46 -0700

    Update theme

--- flash_chat/lib/components/animated_app_title.dart
+++ flash_chat/lib/components/animated_app_title.dart
@@ -4,14 +4,12 @@ import 'package:flutter/material.dart';
 class AnimatedAppTitle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    double unitHeightValue = MediaQuery.of(context).size.height * 0.01;
-    double multiplier = 6.5;
     return TypewriterAnimatedTextKit(
       speed: Duration(milliseconds: 200),
       repeatForever: true,
       text: ['Flash Chat'],
       textStyle: TextStyle(
-        fontSize: multiplier * unitHeightValue,
+        fontSize: 40,
         fontWeight: FontWeight.w900,
       ),
     );
