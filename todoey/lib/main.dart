54db3b7579024dad2d5d1c7340590fce71094db7
Trevor Davis <tzendou@gmail.com>
2020-08-19 22:23:16 -0700

    Add task screen

--- todoey/lib/main.dart
+++ todoey/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoey/screens/task_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,12 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text('Todoey'),
-        ),
-      ),
+      home: TaskScreen(),
     );
   }
 }
